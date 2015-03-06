feeeeb6f8e590a687f71220bee150ba4b78fe91a
James Darling <james@abscond.org>
2015-03-06 16:25:45 +0000

    remove unused endpoints

--- web.rb
+++ web.rb
@@ -5,18 +5,6 @@ require_relative 'models/pingdom_api.rb'
 require_relative 'models/traffic_spike.rb'
 require_relative 'lib/real_time_analytics.rb'
 
-
-get '/appsdown.json' do
-	content_type :json
-	PingdomApi.new.appsdown
-end
-
-get '/appsdownredis.json' do
-	content_type :json
-	PingdomApi.new.appsdownredis
-end
-
-
 post '/notify' do
   PingdomApi.new.notify(params[:payload])
 end
@@ -30,9 +18,4 @@ end
 get '/' do
 	@alerts = Alert.fetch_all
 	erb :index
-end
-
-get	'/traffic_spikes' do
-	TrafficSpike.update
-	"updated"
 end
