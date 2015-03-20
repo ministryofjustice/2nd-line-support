274706fad265ef482396464e3cc5035c2e7dc7c1
Himal Mandalia <himalmandalia@gmail.com>
2015-03-20 20:03:02 +0000

    Update to read from new rota document

--- services/whos_on_duty.rb
+++ services/whos_on_duty.rb
@@ -3,22 +3,20 @@ require 'csv'
 module WhosOnDuty
 
   def self.list
-    response = Excon.get(data_url, headers: { "Accept" => "text/csv" } )
+    response = Excon.get(data_url, headers: { 'Accept' => 'text/csv' } )
 
     begin
-      list = CSV.parse(response.body).first.map(&:strip)
-      if list.is_a?(Array)
-        list
-      else
-        []
-      end
+      members = CSV.parse(response.body)[1][1..-1]
+      members = members.compact.sort.map(&:strip)
+      members.is_a?(Array) ? members : []
     rescue
       []
     end
   end
 
   def self.data_url
-    key = '1HOQMB1zyTaWzbHOS54NzqWUcT3umxAUwwzQAQlDy3a0'
-    "https://docs.google.com/spreadsheet/pub?key=#{key}&single=true&gid=0&output=csv"
+    key = '1j28ELnPgKi0fO6io6aQd-ROUlbXBaiEo63ct4WQVtUQ'
+    gid = '1997221201'
+    "https://docs.google.com/spreadsheet/pub?key=#{key}&single=true&gid=#{gid}&output=csv"
   end
 end
