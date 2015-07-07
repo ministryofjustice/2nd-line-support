3fe3affdaefce8fff627606872008f022762feb4
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2015-07-07 17:34:24 +0100

    gulp amends

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,47 @@
 var gulp = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  source = require('vinyl-source-stream'),
   babel = require('gulp-babel'),
   babelify = require('babelify'),
   browserify = require('browserify'),
-  source = require('vinyl-source-stream'),
   concat = require('gulp-concat'),
   postcss = require('gulp-postcss'),
-  sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('autoprefixer'),
   lost = require('lost');
 
 var paths = {
-  cssSource: 'src/css/',
-  cssDestination: 'public/css/'
+  cssSrc: 'src/css/',
+  cssDest: 'public/css/',
+  jsSrc: 'src/js/',
+  jsDest: 'public/js/'
 };
 
-gulp.task('modules', function() {
-    browserify({
-      entries: './src/js/app.js',
-      debug: true
-    })
-    .transform(babelify)
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./public/js'));
+gulp.task('es6', function() {
+  browserify({
+    entries: './src/js/app.js',
+    debug: true
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest(paths.jsDest));
 });
 
 gulp.task('css', function() {
-  return gulp.src(paths.cssSource + '**/*.css')
+  return gulp.src(paths.cssSrc + '**/*.css')
     .pipe(sourcemaps.init())
     .pipe(postcss([
       lost(),
       autoprefixer()
     ]))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.cssDestination));
+    .pipe(gulp.dest(paths.cssDest));
 });
 
-gulp.watch(paths.cssSource + '**/*.css', ['css']);
+gulp.task('watch', function() {
+  gulp.watch(paths.jsSrc, ['es6']);
+  gulp.watch(paths.cssSrc, ['css']);
+});
 
-gulp.task('default', ['css', 'modules']);
+gulp.task('default', ['watch','css', 'es6']);
+gulp.task('build', ['css', 'es6']);
